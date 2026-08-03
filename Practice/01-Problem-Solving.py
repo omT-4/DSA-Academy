d7054e836c9e0073a62eb458138bdda95f3c2569
omT-4 <omtaware04@gmail.com>
2026-08-03 20:22:07 +0530

    Learn problem-solving mindset and interview approach

---
+++ Practice/01-Problem-Solving.py
@@ -0,0 +1,16 @@
+"""
+Lesson 1
+
+Problem Solving Mindset
+
+No code for this lesson.
+Focused on understanding the software engineering problem-solving process.
+
+Framework:
+
+1. Read
+2. Understand
+3. Brute Force
+4. Optimize
+5. Code
+"""
